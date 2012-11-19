ebe05a586d3a89c9669e0a0382b6e2d9e0eee499
Gregory Ray <gregory.james.ray@gmail.com>
2012-11-19 17:25:46 +0800

    f

--- stopper.sh
+++ stopper.sh
@@ -3,6 +3,5 @@ if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 
 then
         export NODE_ENV=production
         export PATH=/usr/local/bin:$PATH
-        forever stop app.js aapl -l /var/logs/appl-date +"%m-%d-%Y".log > /dev/null
-        forever stop app.js plxs -l /var/logs/plxs-date +"%m-%d-%Y".log > /dev/null
+        forever stop app.js > /dev/null
 fi
