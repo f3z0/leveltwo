03191a3f0a25284413441d965245fcba86fd868a
root <root@localhost.(none)>
2012-11-19 05:42:21 -0500

    f

--- starter.sh
+++ starter.sh
@@ -3,6 +3,6 @@ if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 
 then
         export NODE_ENV=production
         export PATH=/usr/local/bin:$PATH
-        forever start app.js aapl -l /var/logs/appl-date +"%m-%d-%Y".log > /dev/null
-        forever start app.js plxs -l /var/logs/plxs-date +"%m-%d-%Y".log > /dev/null
+        forever -al/var/logs/appl-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js aapl > /dev/null
+        forever -al/var/logs/plxs-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js plxs > /dev/null
 fi
--- stopper.sh
+++ stopper.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
-then
-        export NODE_ENV=production
-        export PATH=/usr/local/bin:$PATH
-        forever stop app.js > /dev/null
-fi
+export PATH=/usr/local/bin:$PATH
+forever stop /var/apps/leveltwo/app.js > /dev/stdout
