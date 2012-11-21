734ac2512528480a634e53b6ced914285ffe177f
Gregory Ray <gregory.james.ray@gmail.com>
2012-11-21 22:10:46 +0800

    f

--- starter.sh
+++ starter.sh
@@ -4,81 +4,85 @@ then
         export NODE_ENV=production
         export PATH=/usr/local/bin:$PATH
         
-        forever --spinSleepTime 5000 -al/var/logs/prov-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js prov > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/AONE-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js AONE > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/clne-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js clne > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/LNET-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js LNET > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/jzj-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js jzj > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/ANTH-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ANTH > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/fnb-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js fnb > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/SVNT-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js SVNT > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/acat-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js acat > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/JRCC-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js JRCC > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/ntn-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ntn > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/ACAD-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ACAD > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/pdli-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js pdli > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/OCLR-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js OCLR > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/eqy-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js eqy > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/VVUS-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js VVUS > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/cnmd-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js cnmd > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/NIHD-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js NIHD > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/acxm-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js acxm > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/GLUU-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js GLUU > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/goodp-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js goodp > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/VELT-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js VELT > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/ubp^f-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ubp^f > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/LXRX-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js LXRX > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/jce-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js jce > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/IDIX-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js IDIX > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/h-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js h > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/ITMN-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ITMN > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/utl-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js utl > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/AVNR-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js AVNR > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/cir-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js cir > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/SQNM-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js SQNM > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/nbix-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js nbix > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/LEAP-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js LEAP > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/nxn-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js nxn > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/ACHN-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ACHN > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/iep-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js iep > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/OREX-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js OREX > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/jva-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js jva > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/SREV-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js SREV > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/clf-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js clf > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/NFLX-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js NFLX > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/mspd-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js mspd > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/NKTR-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js NKTR > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/mil-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js mil > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/CRUS-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js CRUS > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/prxi-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js prxi > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/CLWR-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js CLWR > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/gep-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js gep > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/ISIS-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ISIS > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/nbtf-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js nbtf > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/ARNA-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ARNA > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/hh-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js hh > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/FCEL-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js FCEL > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/vglt-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js vglt > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/DNDN-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js DNDN > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/ubsi-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ubsi > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/HMSY-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js HMSY > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/line-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js line > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/ENTR-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ENTR > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/fax-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js fax > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/PANL-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js PANL > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/renn-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js renn > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/MAKO-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js MAKO > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/gne-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js gne > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/RIMM-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js RIMM > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/patr-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js patr > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/WPRT-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js WPRT > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/gpn-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js gpn > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/ARRY-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ARRY > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/kcc-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js kcc > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/QCOR-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js QCOR > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/pcg^a-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js pcg^a > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/GMCR-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js GMCR > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/ccclw-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js ccclw > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/SWHC-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js SWHC > /dev/stdout 2>&1
         sleep 0.25
-        forever --spinSleepTime 5000 -al/var/logs/dysl-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js dysl > /dev/stdout 2>&1
+        forever --spinSleepTime 5000 -al/var/logs/LSCC-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js LSCC > /dev/stdout 2>&1
+        sleep 0.25
+        forever --spinSleepTime 5000 -al/var/logs/bmti-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js bmti > /dev/stdout 2>&1
+        sleep 0.25
+        forever --spinSleepTime 5000 -al/var/logs/rcon-$(date +"%m-%d-%Y").log start /var/apps/leveltwo/app.js rcon > /dev/stdout 2>&1
 fi
